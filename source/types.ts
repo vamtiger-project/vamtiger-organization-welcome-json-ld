d1acc8d6903276cedbff10f7e1bf8f1614d13141
James-Alex Matthews <vamtiger@vamtiger.com>
2019-06-13 01:01:51 +0200

    vamtiger-node-typescript-commit

--- source/types.ts
+++ source/types.ts
@@ -13,4 +13,4 @@ export interface ILoadScript {
     data: IAnyObject;
 }
 
-export const url = 'https://unpkg.com/vamtiger-organization-welcome-json-ld';
+export const url = 'https://cdn.jsdelivr.net/npm/vamtiger-organization-welcome-json-ld@latest/build/vamtiger-organization-welcome-json-ld.js';
